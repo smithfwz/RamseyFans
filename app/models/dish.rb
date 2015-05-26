1c4b5c12f471adbbfb0403e069e688999bbb571f
smith <smith@futueworkz.com>
2015-05-26 12:58:36 +0700

    Write action search for dishes view

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,5 +2,5 @@ class Dish < ActiveRecord::Base
 	validates :title, :description, presence: true
 	validates :cost, presence: true, numericality: {greater_than: 0}
 
-	scope :search_by, ->(keyword){ where("title ILIKE ?", "%#{keyword}%") }
+	scope :search_by, ->(keyword){ where("title ILIKE ? OR description ILIKE ?", "%#{keyword}%", "%#{keyword}%")}
 end
