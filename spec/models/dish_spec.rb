ae2ffbf42748315932fb48caee60ff115d6d0660
smith <smith@futueworkz.com>
2015-05-27 21:04:31 +0700

    Update test case dish model

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Dish, type: :model do
 	context 'Validation' do 
 		it { should validate_presence_of :title }
 		it { should validate_presence_of :description }
+		it { should validate_numericality_of(:cost).is_greater_than(0) }
+		it { should validate_numericality_of(:pax).is_greater_than(0).only_integer }
 	end
 
 	context 'search_by' do 
