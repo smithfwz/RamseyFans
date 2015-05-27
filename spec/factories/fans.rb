bf99ce938039bae35b3e890ae98b801d4d2bd4ef
smith <smith@futueworkz.com>
2015-05-27 09:44:03 +0700

    Write test case for devise views with action sign up and login

--- spec/factories/fans.rb
+++ spec/factories/fans.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :fan do
-    
+    email 'smith@futureworkz.com'
+    password '123123123'
+    confirmed_at Time.now
   end
 
 end
--- spec/features/devise_spec.rb
+++ spec/features/devise_spec.rb
@@ -1,24 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe 'User can sign up' do 
+RSpec.describe 'Fan can sign up' do 
 
-	describe 'User can sign up' do 
-
-
+	describe 'Fan can sign up' do 
 		it 'sign up' do
 			visit '/'
 
 			click_on 'Sign up'
 
-			expect(page).not_to have_content 'smith@futureworkz.com'
+			expect(Fan.all.count).to eq 0
 
 			fill_in 'fan[email]', with: 'smith@futureworkz.com'
 			fill_in 'fan[password]', with: '123123123'
 			fill_in 'fan[password_confirmation]', with: '123123123'
 
 			click_on 'Register'
+
+			expect(Fan.all.count).to eq 1
+			expect(page).to have_content 'Please follow the link to activate your account.'
+		end
+	end
+
+	describe 'Fan can login' do 
+		let!(:fan_smith) { create(:fan) }
+		it 'login' do 
+			visit '/'
+
+			click_on 'Login'
+
+			expect(page).not_to have_content 'Signed in successfully.'
+
+			fill_in 'fan[email]', with: 'smith@futureworkz.com'
+			fill_in 'fan[password]', with: '123123123'
+
+			click_on 'Log in'
 			
-			expect(page).to have_content 'Welcome! You have signed up successfully.'
+			expect(page).to have_content 'Signed in successfully.'
 		end
 	end
 end
