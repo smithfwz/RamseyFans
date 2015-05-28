7edb552cfeeebf3ac69c1d878633cdbfaec31698
smith <smith@futueworkz.com>
2015-05-28 13:59:05 +0700

    Update fan model

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,5 +1,10 @@
 class Fan < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, 
+  			 :registerable, 
+  			 :rememberable, 
+  			 :trackable, 
+  			 :validatable, 
+  			 :confirmable
 end
