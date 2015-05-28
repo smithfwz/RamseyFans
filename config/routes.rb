e9190e12cec511e1fe38f6b8b15edab434b7dd20
smith <smith@futueworkz.com>
2015-05-28 18:14:56 +0700

    Config routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :fans
   resources :dishes
+  resources :my_dishes
   
   root to: "dishes#index"
   # The priority is based upon order of creation: first created -> highest priority.
