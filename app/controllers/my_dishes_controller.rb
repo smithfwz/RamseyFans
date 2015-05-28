7a9e242b67b7f5702f41a0133cbc791270be5392
smith <smith@futueworkz.com>
2015-05-28 21:52:20 +0700

    Update MyDishesController with action edit and update

--- app/controllers/my_dishes_controller.rb
+++ app/controllers/my_dishes_controller.rb
@@ -1,10 +1,10 @@
 class MyDishesController < ApplicationController
 	before_action :authenticate_fan!
-  # before_action :check_ownership_of_my_dish!, only: [:edit, :update]
+  before_action :check_ownership_of_my_dish!, only: [:edit, :update]
 
   def index
     @my_dishes = Dish.where(fan_id: fan_id)
-    @my_dishes = @my_dishes.order(title: :desc)
+    @my_dishes = @my_dishes.order(title: :asc)
   end
 
   def new
@@ -22,7 +22,27 @@ class MyDishesController < ApplicationController
     end
   end
 
+  def edit
+    @my_dish = get_my_dish
+    render :new
+  end
+
+  def update
+    @my_dish = get_my_dish
+    if @my_dish.update(my_dish_params)
+      flash[:notice] = "You have Successfully Update your dish"
+      redirect_to my_dishes_url
+    else
+      flash.now[:alert] = "There's an error on updating, please try again."
+      render :new
+    end
+  end
+
   private
+
+  def dish_id
+    params.require(:id)
+  end
 
   def fan_id
     current_fan.id
@@ -30,5 +50,15 @@ class MyDishesController < ApplicationController
 
   def my_dish_params
     params.require(:dish).permit(:title, :description, :cost, :pax, :vegetarian, :fan_id)
+  end
+
+  def get_my_dish
+    @my_dish ||= Dish.find(dish_id)
+  end
+
+  def check_ownership_of_my_dish!
+    if current_fan.id != get_my_dish.fan_id
+      redirect_to my_dishes_url, notice: "You do not have the permission"
+    end
   end
 end
